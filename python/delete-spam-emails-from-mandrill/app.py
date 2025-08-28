6d2576f9b84a1e8b87cf75b943c602a9fa9393ee
Salman Waheed <mkdirenv@gmail.com>
2025-08-28 19:38:40 +0500

    improve delete-spam-emails-from-mandrill python script

--- python/delete-spam-emails-from-mandrill/app.py
+++ python/delete-spam-emails-from-mandrill/app.py
@@ -1,14 +1,17 @@
 import requests
 
+# https://mailchimp.com/developer/transactional/api/rejects
+# curl -X GET "https://mandrillapp.com/api/1.0/rejects/list" -H "content-type: application/json" -d '{"key": ""}'
+
 API_KEY = ""
 BASE_URL = "https://mandrillapp.com/api/1.0/rejects"
 REASON = ["spam", "unsub"]
 
-data = {
-  "key": API_KEY
-}
+timeout = 30
+data = {"key": API_KEY}
+headers = {"content-type": "application/json"}
 
-resp = requests.get(f"{BASE_URL}/list.json", json=data)
+resp = requests.get(f"{BASE_URL}/list", json=data, headers=headers, timeout=timeout)
 emails = resp.json()
 
 for x in emails:
@@ -16,4 +19,4 @@ for x in emails:
     email = x['email']
     print(f"Removing spam email: {email}")
     data.update({"email": email})
-    requests.post(f"{BASE_URL}/delete.json", json=data)
+    requests.post(f"{BASE_URL}/delete", json=data, headers=headers, timeout=timeout)
